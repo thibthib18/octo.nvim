2da2516bb69352e7e26326974120d6361f0cf153
thib <thibault@seervision.com>
2021-08-03 16:41:07 +0200

    add write mr details

--- lua/octo/writers.lua
+++ lua/octo/writers.lua
@@ -274,6 +274,186 @@ function M.write_reactions(bufnr, reaction_groups, line)
   end
 end
 
+function M.write_mr_details(bufnr, mr, update)
+  -- clear virtual texts
+  vim.api.nvim_buf_clear_namespace(bufnr, constants.OCTO_DETAILS_VT_NS, 0, -1)
+
+  local details = {}
+  local buffer = octo_buffers[bufnr]
+
+  -- repo
+  if buffer then
+    local repo_vt = {
+      {"Repo: ", "OctoDetailsLabel"},
+      {" " .. buffer.repo, "OctoDetailsValue"} -- TODO: change to gitlab icon
+    }
+    table.insert(details, repo_vt)
+  end
+
+  -- author
+  local author_vt = {{"Created by: ", "OctoDetailsLabel"}}
+  local conf = config.get_config()
+  local author_bubble = bubbles.make_user_bubble(
+    mr.author.name,
+    mr.author.username == conf.username
+  )
+
+  vim.list_extend(author_vt, author_bubble)
+  table.insert(details, author_vt)
+
+  add_details_line(details, "Created", mr.createdAt, "date")
+  if mr.state == "closed" then -- TODO:make enums for those states
+    if mr.closedAt then -- closedAt is only for issues not for MRs
+      add_details_line(details, "Closed", mr.closedAt, "date")
+    end
+  else
+    add_details_line(details, "Updated", mr.updatedAt, "date")
+  end
+
+  -- assignees
+  local assignees_vt = {
+    {"Assignees: ", "OctoDetailsLabel"}
+  }
+  if mr.assignees and #mr.assignees.nodes > 0 then
+    for _, assignee in ipairs(mr.assignees.nodes) do
+      local isViewer = true
+      local user_bubble = bubbles.make_user_bubble(assignee.name, isViewer, { margin_width = 1 })
+      vim.list_extend(assignees_vt, user_bubble)
+    end
+  else
+    table.insert(assignees_vt, {"No one assigned ", "OctoMissingDetails"})
+  end
+  table.insert(details, assignees_vt)
+
+  -- labels
+  local labels_vt = {
+    {"Labels: ", "OctoDetailsLabel"}
+  }
+  if #mr.labels.nodes > 0 then
+    for _, label in ipairs(mr.labels.nodes) do
+      local label_bubble = bubbles.make_label_bubble(
+        label.title,
+        label.color,
+        { right_margin_width = 1 }
+      )
+      vim.list_extend(labels_vt, label_bubble)
+    end
+  else
+    table.insert(labels_vt, {"None yet", "OctoMissingDetails"})
+  end
+  table.insert(details, labels_vt)
+
+  -- additional details for pull requests
+  if mr.diffStatsSummary.fileCount then
+    -- reviewers
+    local reviewers = {}
+    local collect_reviewer = function (name, state)
+      --if vim.g.octo_viewer ~= name then
+        if not reviewers[name] then
+          reviewers[name] = {state}
+        else
+          local states = reviewers[name]
+          if not vim.tbl_contains(states, state) then
+            table.insert(states, state)
+          end
+          reviewers[name] = states
+        end
+      --end
+    end
+    -- collect reviewers
+    for _, reviewer in ipairs(mr.reviewers.nodes) do
+      -- TODO: review states are pretty different between GH and GL
+      local state = "COMMENTED"
+      if reviewer.mergeRequestInteraction.reviewState == "UNREVIEWED" then
+        state = "REVIEW_REQUIRED"
+      end
+      collect_reviewer(reviewer.name, state)
+    end
+
+    -- collect assignees
+    for _, assignee in ipairs(mr.assignees.nodes) do
+      collect_reviewer(assignee.name, "PENDING")
+    end
+
+    local reviewers_vt = {
+      {"Reviewers: ", "OctoDetailsLabel"}
+    }
+    if #vim.tbl_keys(reviewers) > 0 then
+      for _, name in ipairs(vim.tbl_keys(reviewers)) do
+        local strongest_review = utils.calculate_strongest_review_state(reviewers[name])
+        local reviewer_vt = {
+          {name , "OctoUser"},
+          {" "},
+          {utils.state_icon_map[strongest_review], utils.state_hl_map[strongest_review]},
+          {" "},
+        }
+        vim.list_extend(reviewers_vt, reviewer_vt)
+      end
+    else
+      table.insert(reviewers_vt, {"No reviewers", "OctoMissingDetails"})
+    end
+    table.insert(details, reviewers_vt)
+
+    -- merged_by
+    if mr.state == "merged" then
+      local merged_by_vt = {{"Merged by: ", "OctoDetailsLabel"}}
+      local name = mr.mergeUser.name
+      local is_viewer = conf.username == name
+      local user_bubble = bubbles.make_user_bubble(name, is_viewer)
+      vim.list_extend(merged_by_vt, user_bubble)
+      table.insert(details, merged_by_vt)
+    end
+
+    -- from/into branches
+    local branches_vt = {
+      {"From: ", "OctoDetailsLabel"},
+      {mr.sourceBranch, "OctoDetailsValue"},
+      {" Into: ", "OctoDetailsLabel"},
+      {mr.targetBranch, "OctoDetailsValue"}
+    }
+    table.insert(details, branches_vt)
+
+    -- changes
+    local changes_vt = {
+      {"Commits: ", "OctoDetailsLabel"},
+      {tostring(mr.commitCount), "OctoDetailsValue"},
+      {" Changed files: ", "OctoDetailsLabel"},
+      {tostring(mr.diffStatsSummary.fileCount), "OctoDetailsValue"},
+      {" (", "OctoDetailsLabel"},
+      {string.format("+%d ", mr.diffStatsSummary.additions), "OctoDiffstatAdditions"},
+      {string.format("-%d ", mr.diffStatsSummary.deletions), "OctoDiffstatDeletions"}
+    }
+    local diffstat = utils.diffstat({additions = mr.diffStatsSummary.additions, deletions = mr.diffStatsSummary.deletions})
+    if diffstat.additions > 0 then
+      table.insert(changes_vt, {string.rep("■", diffstat.additions), "OctoDiffstatAdditions"})
+    end
+    if diffstat.deletions > 0 then
+      table.insert(changes_vt, {string.rep("■", diffstat.deletions), "OctoDiffstatDeletions"})
+    end
+    if diffstat.neutral > 0 then
+      table.insert(changes_vt, {string.rep("■", diffstat.neutral), "OctoDiffstatNeutral"})
+    end
+    table.insert(changes_vt, {")", "OctoDetailsLabel"})
+    table.insert(details, changes_vt)
+  end
+
+  local line = 3
+  -- write #details + empty lines
+  local empty_lines = {}
+  for _ = 1, #details + 1 do
+    table.insert(empty_lines, "")
+  end
+  if not update then
+    M.write_block(bufnr, empty_lines, line)
+  end
+
+  -- write details as virtual text
+  for _, d in ipairs(details) do
+    M.write_virtual_text(bufnr, constants.OCTO_DETAILS_VT_NS, line - 1, d)
+    line = line + 1
+  end
+end
+
 function M.write_details(bufnr, issue, update)
   -- clear virtual texts
   vim.api.nvim_buf_clear_namespace(bufnr, constants.OCTO_DETAILS_VT_NS, 0, -1)
